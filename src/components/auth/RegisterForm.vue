2d70ea4bd17c42235a46dc6770a546c0040045ae
hazhiiir <hazhiiir@gmail.com>
2020-09-11 19:55:22 +0430

    Add cliend-side validation fro register fields

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -6,10 +6,12 @@
           id="register-input-group-1"
           label="User"
           label-for="input-1"
+          :invalid-feedback="userNameInvalidFeedback"
+          :state="!$v.newUser.username.$error"
         >
           <b-form-input
             id="input-1"
-            v-model="newUser.username"
+            v-model="$v.newUser.username.$model"
             type="text"
             required
           ></b-form-input>
@@ -18,10 +20,12 @@
           id="register-input-group-2"
           label="Email"
           label-for="input-2"
+          :invalid-feedback="emailInvalidFeedback"
+          :state="!$v.newUser.email.$error"
         >
           <b-form-input
             id="input-2"
-            v-model="newUser.email"
+            v-model="$v.newUser.email.$model"
             type="email"
             required
           ></b-form-input>
@@ -30,15 +34,24 @@
           id="register-input-group-3"
           label="Password"
           label-for="input-3"
+          :state="!$v.newUser.password.$error"
+          :invalid-feedback="passwordInvalidFeedback"
         >
           <b-form-input
             id="input-3"
-            v-model="newUser.password"
+            v-model="$v.newUser.password.$model"
             type="password"
             required
           ></b-form-input>
         </b-form-group>
-        <b-button type="submit" variant="primary" block>Register</b-button>
+        <b-button
+          type="submit"
+          variant="primary"
+          block
+          :disabled="$v.newUser.$error"
+          @click="() => console.log('test')"
+          >Register</b-button
+        >
       </b-form>
     </div>
   </form-container>
@@ -46,15 +59,30 @@
 
 <script lang="ts">
 import FormContainer from "./FormContainer.vue";
+import { validationMixin } from "vuelidate";
+import { required, email } from "vuelidate/lib/validators";
 import { Component, Vue } from "vue-property-decorator";
 import { User } from "../../store/types";
 
 @Component({
+  mixins: [validationMixin],
   components: {
     FormContainer
+  },
+  validations: {
+    newUser: {
+      username: { required },
+      email: {
+        required,
+        email
+      },
+      password: {
+        required
+      }
+    }
   }
 })
-export default class HelloWorld extends Vue {
+export default class RegisterForm extends Vue {
   private newUser: User = {
     email: "",
     username: "",
@@ -74,6 +102,25 @@ export default class HelloWorld extends Vue {
         title: "Login"
       }
     };
+  }
+  get userNameInvalidFeedback() {
+    if (!this?.$v?.newUser?.username?.required) {
+      return "Required Field";
+    } else return "";
+  }
+  get emailInvalidFeedback() {
+    if (!this?.$v?.newUser?.email?.required) {
+      return "Required Field";
+    } else if (!this?.$v?.newUser?.email?.email) {
+      return "Invalid Format";
+    } else {
+      return "";
+    }
+  }
+  get passwordInvalidFeedback() {
+    if (!this?.$v?.newUser?.password?.required) {
+      return "Required Field";
+    } else return "";
   }
   onSubmit() {
     return;
