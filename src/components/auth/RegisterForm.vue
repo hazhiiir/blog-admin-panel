062ad6f1adf2ff07a6f02637171d8a690f884b11
hazhiiir <hazhiiir@gmail.com>
2020-09-14 00:01:45 +0430

    Wire register component to store
    Move all texts to translations module
    
    Refactor error handling process

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,73 +1,101 @@
 <template>
-  <form-container title="Register" :secondaryAction="secondaryAction">
-    <div class="register-form text-left">
-      <b-form @submit="onSubmit" @reset="onReset">
-        <b-form-group
-          id="register-input-group-1"
-          label="User"
-          label-for="input-1"
-          :invalid-feedback="userNameInvalidFeedback"
-          :state="!$v.newUser.username.$error"
-        >
-          <b-form-input
-            id="input-1"
-            v-model="$v.newUser.username.$model"
-            type="text"
-            required
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          id="register-input-group-2"
-          label="Email"
-          label-for="input-2"
-          :invalid-feedback="emailInvalidFeedback"
-          :state="!$v.newUser.email.$error"
-        >
-          <b-form-input
-            id="input-2"
-            v-model="$v.newUser.email.$model"
-            type="email"
-            required
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          id="register-input-group-3"
-          label="Password"
-          label-for="input-3"
-          :state="!$v.newUser.password.$error"
-          :invalid-feedback="passwordInvalidFeedback"
-        >
-          <b-form-input
-            id="input-3"
-            v-model="$v.newUser.password.$model"
-            type="password"
-            required
-          ></b-form-input>
-        </b-form-group>
-        <b-button
-          type="submit"
-          variant="primary"
-          block
-          :disabled="$v.newUser.$error"
-          @click="() => console.log('test')"
-          >Register</b-button
-        >
-      </b-form>
-    </div>
-  </form-container>
+  <div class="register-form-root-container">
+    <alert ref="alertComponent" :variant="alertVariant">
+      <span v-if="success"
+        >You are registered successfully. redirecting to dashboard...</span
+      >
+      <span v-if="requestError">Somthing wrong!</span>
+    </alert>
+    <form-container
+      :title="$t('auth.register.title')"
+      :secondaryAction="secondaryAction"
+    >
+      <div class="register-form text-left">
+        <b-form @submit.prevent="onSubmit" @reset="onReset">
+          <b-form-group
+            id="register-input-group-1"
+            :label="$t('auth.register.fields.username.label')"
+            label-for="input-1"
+            :invalid-feedback="invalidFeedbackMessage('username')"
+            :state="!hasError('username')"
+          >
+            <b-form-input
+              id="input-1"
+              v-model="$v.newUser.username.$model"
+              type="text"
+              :placeholder="$t('auth.register.fields.username.placeholder')"
+              required
+              @keyup="flushRequestError('username')"
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group
+            id="register-input-group-2"
+            :label="$t('auth.register.fields.email.label')"
+            label-for="input-2"
+            :invalid-feedback="invalidFeedbackMessage('email')"
+            :state="!hasError('email')"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="$v.newUser.email.$model"
+              type="email"
+              :placeholder="$t('auth.register.fields.email.placeholder')"
+              required
+              @keyup="flushRequestError('email')"
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group
+            id="register-input-group-3"
+            :label="$t('auth.register.fields.password.label')"
+            label-for="input-3"
+            :state="!hasError('password')"
+            :invalid-feedback="invalidFeedbackMessage('password')"
+          >
+            <b-form-input
+              id="input-3"
+              v-model="$v.newUser.password.$model"
+              type="password"
+              required
+              @keyup="flushRequestError('password')"
+            ></b-form-input>
+          </b-form-group>
+          <b-button v-if="waiting" variant="primary" disabled block>
+            <b-spinner small type="grow"></b-spinner>
+            {{ $t("auth.register.loadingButton") }}
+          </b-button>
+          <b-button
+            v-if="!waiting"
+            type="button"
+            variant="primary"
+            block
+            :disabled="$v.newUser.$error"
+            @click="onSubmit()"
+            >{{ $t("auth.register.button") }}
+          </b-button>
+        </b-form>
+      </div>
+    </form-container>
+  </div>
 </template>
 
 <script lang="ts">
 import FormContainer from "./FormContainer.vue";
 import { validationMixin } from "vuelidate";
 import { required, email } from "vuelidate/lib/validators";
-import { Component, Vue } from "vue-property-decorator";
-import { User } from "../../store/types";
+import { Action, State, Mutation, namespace } from "vuex-class";
+import { Component, Vue, Ref } from "vue-property-decorator";
+import { User, RegisterFields, ValidationError } from "../../store/types";
+import Alert, { AlertVariantProp } from "../common/Alert.vue";
+
+const authState = namespace("auth", State);
+const authAction = namespace("auth", Action);
+const authMutaion = namespace("auth", Mutation);
 
 @Component({
   mixins: [validationMixin],
   components: {
-    FormContainer
+    FormContainer,
+    Alert
   },
   validations: {
     newUser: {
@@ -89,42 +117,90 @@ export default class RegisterForm extends Vue {
     password: ""
   };
 
+  private validationMap = {
+    username: ["required"],
+    email: ["required", "email"],
+    password: ["required"]
+  };
+
+  private secondaryAction = {
+    description: this.$t("auth.register.secondaryAction.desctiption"),
+    href: this.$t("auth.register.secondaryAction.href"),
+    title: this.$t("auth.register.secondaryAction.title")
+  };
+
+  @Ref() alertComponent!: Alert;
   data() {
     return {
       form: {
         email: "",
         user: "",
         password: ""
-      },
-      secondaryAction: {
-        description: "Already Registered?",
-        href: "/",
-        title: "Login"
       }
     };
   }
-  get userNameInvalidFeedback() {
-    if (!this?.$v?.newUser?.username?.required) {
-      return "Required Field";
-    } else return "";
+
+  @authState("error") requestError!: ValidationError<RegisterFields> | null;
+  @authState("waiting") waiting!: boolean;
+  @authState("success") success!: boolean;
+  @authAction registerUser: any;
+  @authMutaion("flushError") flushRequestError: any;
+
+  get userNameHasError() {
+    return !(
+      (this.requestError && Boolean(this.requestError.username)) ||
+      this?.$v?.newUser?.username?.$error
+    );
   }
-  get emailInvalidFeedback() {
-    if (!this?.$v?.newUser?.email?.required) {
-      return "Required Field";
-    } else if (!this?.$v?.newUser?.email?.email) {
-      return "Invalid Format";
+
+  get alertVariant(): AlertVariantProp {
+    if (this.requestError) {
+      return "danger";
     } else {
-      return "";
+      return "success";
     }
   }
-  get passwordInvalidFeedback() {
-    if (!this?.$v?.newUser?.password?.required) {
-      return "Required Field";
-    } else return "";
+
+  invalidFeedbackMessage(field: keyof RegisterFields) {
+    if (this.requestError && this.requestError[field]) {
+      return this.requestError[field];
+    } else {
+      const validations = this.validationMap[field];
+      const fieldValidationState = this.$v.newUser[field];
+
+      for (let i = 0; i < validations.length; ++i) {
+        const currentRule = validations[i];
+        if (
+          typeof fieldValidationState !== "undefined" &&
+          !fieldValidationState[currentRule]
+        ) {
+          return this.$t(`auth.fieldValidation.${currentRule}`);
+        }
+      }
+    }
   }
+
+  hasError(field: keyof RegisterFields): boolean {
+    return Boolean(
+      (this.requestError && Boolean(this.requestError[field])) ||
+        this?.$v?.newUser?.[field]?.$error
+    );
+  }
+
   onSubmit() {
-    return;
+    this.registerUser(this.newUser).then(
+      () => {
+        this.alertComponent.showAlert();
+        setTimeout(() => {
+          this.$router.push("/");
+        }, 4000);
+      },
+      () => {
+        this.alertComponent.showAlert();
+      }
+    );
   }
+
   onReset() {
     return;
   }
