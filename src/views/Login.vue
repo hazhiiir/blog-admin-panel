9a95ba6074393820a6656659ec2309d264548364
hazhiiir <hazhiiir@gmail.com>
2020-09-14 04:29:40 +0430

    Add Login page
    Add /login route

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="register">
+    <login-form></login-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import LoginForm from "../components/auth/LoginForm.vue";
+
+@Component({
+  components: {
+    LoginForm
+  }
+})
+export default class Login extends Vue {}
+</script>
