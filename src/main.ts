82cc49ba3aac7e15ec4056cde41aaf126918d6e3
hazhiiir <hazhiiir@gmail.com>
2020-09-13 23:53:51 +0430

    Add i18n support to project
    Add en local messages

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,23 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import UIKit from "./ui-kit/";
-
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueI18n from "vue-i18n";
 import Api from "@/services/api";
+import translationsMessages from "../config/translations";
 
 Vue.config.productionTip = false;
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: "en",
+  messages: translationsMessages
+});
+
 Api.init();
 UIKit.init();
 
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)
