f701ca6d6aa84171795c2cd3157dcb0f4a092b56
hazhiiir <hazhiiir@gmail.com>
2020-09-15 13:57:02 +0430

    Add article module types to store

--- src/store/types.ts
+++ src/store/types.ts
@@ -2,6 +2,10 @@ export type ValidationError<FieldSet> = {
   [key in keyof FieldSet | "message"]: string;
 };
 
+export interface PagingParameters {
+  limit: number;
+  offset: number;
+}
 export interface RegisterFields {
   email: string;
   password: string;
@@ -27,6 +31,47 @@ export interface AuthState {
   isAuthenticated: boolean;
 }
 
+export interface CRUDOperations {
+  create: "create";
+  read: "read";
+  update: "update";
+  delete: "delete";
+}
+
+export interface Author {
+  username: string;
+  image: string;
+}
+
+export interface Article {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+  tagList: string[];
+  favorited: boolean;
+  favoritesCount: number;
+  author: Author;
+}
+
+export type RequestStateFlagsObject = {
+  [key in keyof CRUDOperations]: boolean;
+};
+
+export interface ArticleState {
+  waiting: Partial<RequestStateFlagsObject> | null;
+  success: Partial<RequestStateFlagsObject> | null;
+  error: {} | null;
+  entities: {
+    [key: string]: Article;
+  } | null;
+  allIds: string[] | null;
+  articlesCount: number;
+}
+
 export interface RootState {
   auth: AuthState;
+  article: ArticleState;
 }
